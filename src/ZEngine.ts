cddc9f3142c6b886474a3ec4f6d4bc5e8969c429
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2025-02-24 22:33:08 +0700

    initial commit

---
+++ src/ZEngine.ts
@@ -0,0 +1,68 @@
+import WebSocket from "ws";
+
+// biome-ignore lint/suspicious/noExplicitAny: It is complicated
+export type Data = Record<string, any>;
+
+// biome-ignore lint/suspicious/noExplicitAny: <It is complicated>
+type Callback = (oldValue: any, newValue: any, newData: Data) => void;
+type Entry = Map<string, Set<Callback>>;
+
+export default class ZEngine {
+	cache: Data = {};
+	entries: Entry = new Map();
+
+	constructor(url: string) {
+		const ws = new WebSocket(url);
+		ws.on("open", () => console.log("WebSocket connected!"));
+		ws.on("close", () => console.log("WebSocket disconnected!"));
+		ws.on("error", (error) => {
+			ws.close();
+			console.error(error);
+		});
+		ws.on("message", (message) => {
+			const data: Data = JSON.parse(message.toString());
+			this.update(data);
+		});
+	}
+
+	register(key: string, callback: Callback) {
+		if (!this.entries.get(key)) {
+			this.entries.set(key, new Set());
+		}
+		this.entries.get(key)?.add(callback);
+	}
+
+	unregister(key: string, callback: Callback) {
+		this.entries.get(key)?.delete(callback);
+		if (this.entries.get(key)?.size === 0) {
+			this.entries.delete(key);
+		}
+	}
+
+	private update(newData: Data) {
+		for (const key of this.entries.keys()) {
+			const oldValue = this.search(key, this.cache);
+			const newValue = this.search(key, newData);
+			const callbacks = this.entries.get(key);
+
+			if (!callbacks) continue;
+			for (const callback of callbacks)
+				if (oldValue !== newValue) callback(oldValue, newValue, newData);
+		}
+
+		this.cache = newData;
+	}
+
+	private search(key: string, obj: Data) {
+		const attrs = key.split(".");
+		let curr = obj;
+		for (const attr of attrs) {
+			if (!(curr instanceof Object)) return curr;
+			curr = curr[attr];
+		}
+
+		return curr;
+	}
+}
+
+module.exports = ZEngine;
